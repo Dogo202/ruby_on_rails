c8cf050c3cf61dad7599569b8989a156a07c7711
Timur <pistols202@gmail.com>
2024-09-02 13:14:16 +0500

    static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_page/home'
+  get 'static_page/help'
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
