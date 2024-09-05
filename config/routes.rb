52d06194e075a6365348f2cf9180b216d7dfce2a
Timur <pistols202@gmail.com>
2024-09-05 15:47:00 +0500

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'home' => 'static_page#home'
-  get 'help' =>  'static_page#help'
-  get 'about' =>  'static_page#about'
-  get 'contact' =>  'static_page#contact'
-  get 'signup' => 'users#new'
+  get    'home'    => 'static_page#home'
+  get    'help'    =>  'static_page#help'
+  get    'about'   =>  'static_page#about'
+  get    'contact' =>  'static_page#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
