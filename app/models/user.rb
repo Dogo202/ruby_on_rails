c79d25302b8a12338e1e1f7acfcf6fe4ce744cb6
Timur <pistols202@gmail.com>
2024-09-08 14:41:53 +0500

    tmp save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   has_many :microposts
-  before_save{self.email = email.downcase}
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, uniqueness: false ,length: {maximum: 50}
   validates :password, presence: true, uniqueness: false ,length: {minimum: 6} , allow_nil: true
   validates :email, presence: true, uniqueness: { case_sensitive: false}, length: {maximum: 255},format: {with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
@@ -29,4 +30,18 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+  # Переводит адрес электронной почты в нижний регистр.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Создает и присваивает активационнй токен и дайджест.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_05_115444) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_08_093020) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -25,6 +25,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_05_115444) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
