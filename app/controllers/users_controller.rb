aa3e7cb64a307ed522f50d8d349c1a3b5ae503d3
Timur <pistols202@gmail.com>
2024-09-10 18:22:50 +0500

    i think i'm done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page],per_page: 10)
+    redirect_to(root_url) && return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page],per_page: 30)
   end
 
   # GET /users/new
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,7 @@ class User < ActiveRecord::Base
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def feed
-    Micropost.where("user_id = ?", id)
-  end
+
 
   def User.new_token
     SecureRandom.urlsafe_base64
@@ -74,6 +72,16 @@ class User < ActiveRecord::Base
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                   WHERE  follower_id = :user_id"
+
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
 
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
